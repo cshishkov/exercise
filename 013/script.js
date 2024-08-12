be690d01e82fb0923dbb507c7f4e3a38b5ebf622
Chavdar Shishkov <cshishkov@sprintax.com>
2024-08-12 16:16:45 +0300

    Add: Update

--- 013/script.js
+++ 013/script.js
@@ -68,6 +68,7 @@ class AxiosPP {
 const apiClient = new AxiosPP(url);
 
 document.addEventListener("DOMContentLoaded", fetchData());
+const postStates = {};
 
 async function fetchData() {
   const getData = await apiClient.get("posts");
@@ -101,7 +102,7 @@ function calculateDaysDifference(dateString) {
     { label: "minute", seconds: 60 },
   ];
 
-  intervals.map(interval => {
+  intervals.map((interval) => {
     const count = Math.floor(diffInSeconds / interval.seconds);
     if (count >= 1) {
       if (count === 1) {
@@ -123,6 +124,91 @@ function onDecreaseLikesClick(e) {
   e.preventDefaults();
 }
 
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const name = document.getElementById("inputName");
+  const title = document.getElementById("inputTitle");
+  const content = document.getElementById("inputContent");
+  const tagContent = document.querySelectorAll("#tag-el");
+
+  tagContent.forEach((element) => {
+    console.log(element.value);
+  });
+
+  const response = {
+    title: title.value,
+    author: {
+      name: name.value,
+    },
+    content: content.value,
+    tags: Array.from(tagContent, (el) => {
+      if (el.value !== "") {
+        return el.value;
+      }
+    }),
+  };
+
+  console.log(response);
+}
+
+function onAddPostClick(e) {
+  e.preventDefault();
+  const formContainer = document.getElementById("add-post-container");
+  formContainer.innerHTML = addPostForm();
+}
+
+let tagsCount = 1;
+function onAddTagClick(e) {
+  e.preventDefault();
+
+  const tagsWrapper = document.getElementById("tags-wrapper");
+
+  tagsWrapper.insertAdjacentHTML("afterbegin", addTags());
+
+  tagsCount++;
+}
+
+function addTags() {
+  return `
+      <input id="tag-el" type="text" class="form-control tag-element" placeholder="Tag" >
+  `;
+}
+
+const addPostForm = () => {
+  return `
+<div class="col-12">
+    <form id="add-post-form">
+        <div class="form-row">
+            <div class="form-group col-12">
+                <label for="inputName">Name</label>
+                <input type="text" class="form-control" id="inputName" placeholder="Name">
+            </div>
+            <div class="form-group col-12">
+                <label for="inputTitle">Title</label>
+                <input type="text" class="form-control" id="inputTitle" placeholder="Title">
+            </div>
+            <div class="form-group col-12">
+                <label for="inputContent">Content</label>
+                <textarea type="text" class="form-control" id="inputContent" placeholder="Content"></textarea>
+            </div>
+            <div class="col-12">
+                <label for="inputTag">Tags 
+                <img src="./assets/plus-icon.png" alt="" class="comment-icon cursor-pointer" onclick="onAddTagClick(event);"/>
+                </label>
+                <div class="row">
+                    <div id="tags-wrapper" class="col-12 d-flex flex-wrap align-items-center gap-1">
+                        <input id="tag-el" type="text" class="form-control tag-element" placeholder="Tag">
+                    </div>
+                </div>
+            </div>
+        </div>
+          <button type="submit" class="" onclick="handleSubmit(event);">Submit</button>
+    </form>
+</div>
+  `;
+};
+
 function onDeleteClick(e) {
   e.preventDefault();
   const commentElement = e.target.closest(".row");
@@ -137,7 +223,7 @@ function onEditClick(e) {
   const currentText = contentElement.textContent.trim();
 
   contentElement.innerHTML = `
-        <textarea class="form-control edit-textarea">${currentText}</textarea>
+        <textarea class="form-control edit-textarea" rows="5">${currentText}</textarea>
         <div class="edit-actions mt-2">
             <button class="read-more-button save-edit">Save</button>
             <button class="read-more-button cancel-edit">Cancel</button>
@@ -161,6 +247,8 @@ function onSaveEditClick(e) {
 
   if (newText) {
     contentElement.innerHTML = newText;
+  } else {
+    onCancelEditClick(e);
   }
 }
 
@@ -176,16 +264,59 @@ function onCancelEditClick(e) {
 }
 
 function onReplyClick(e) {
-  e.preventDefaults();
+  e.preventDefault();
+
+  const commentElement = e.target.closest(".row");
+  commentElement.insertAdjacentHTML("beforeend", replySection());
 }
 
-const postStates = {};
+function onSubmitReplyClick(e) {
+  e.preventDefault();
 
-const comment = (author, date, content, likes, offset = 0) => {
+  const reply = document.getElementById("inputReply");
+  const commentElement = e.target.closest(".row");
+  // const comment = comment("asd", new Date(), "asdasdasdasdasdasd", 10, 1);
+
+  // commentElement.insertAdjacentHTML(
+  //   "beforeend",
+  //   comment("asd", new Date(), "asdasdasdasdasdasd", 10, 1)
+  // );
+
+  onCancelReplyClick(e);
+}
+
+function onCancelReplyClick(e) {
+  e.preventDefault();
+
+  const replySection = e.target.closest("#reply-comment-section");
+  const reply = document.getElementById("inputReply");
+  reply.textContent = "";
+  replySection.remove();
+}
+
+const replySection = () => {
   return `
-            <div class="row" ${offset ? "" : 'style = "margin-top:20px;"' }>
+<div id="reply-comment-section" class="w-100 px-0 mb--20">
+    <div class="comment col-12">
+        <form>
+            <div class="form-group mb--20">
+                <label for="inputReply">Reply</label>
+                <textarea class="form-control" id="inputReply" rows="3"></textarea>
+            </div>
+            <button type="submit" class="read-more-button" onclick="onSubmitReplyClick(event)">Submit</button>
+            <button class="read-more-button" onclick="onCancelReplyClick(event)">Cancel</button>
+        </form>
+    </div>
+</div>
+  `;
+};
+
+const comment = (author, date, content, likes, offset) => {
+  return `
+            <div class="row" ${offset ? "" : 'style = "margin-top:20px;"'}>
               <div class="comment d-flex col-12 m--y-20 ${
-                offset ? " max--width-90" : "" }">
+                offset ? " max--width-90" : ""
+              }">
                   <div class="d-flex flex-column align-items-center justify-content-between likes-wrapper">
                       <img src="./assets/plus-icon.png" alt="" class="likes-icon" onclick="onIncrementLikesClick(event);" />
                       <p class="user-select-none">${likes}</p>
@@ -198,7 +329,9 @@ const comment = (author, date, content, likes, offset = 0) => {
                                   <img src="./assets/avatar-icon.jpg" alt="" class="avatar-img" />
                                   <h4 class="p-0 m-0">${author}</h4>
                               </span>
-                              <p>${calculateDaysDifference(date)}</p>
+                              <p class="align-self-center">${calculateDaysDifference(
+                                date
+                              )}</p>
                           </span>
                           <span class="d-flex align-items-center ml-auto gap-3 flex-column flex-md-row">
                               <span class="d-flex align-items-center cursor-pointer" onclick="onDeleteClick(event)">
@@ -209,10 +342,7 @@ const comment = (author, date, content, likes, offset = 0) => {
                                   <img src="./assets/edit-icon.png" alt="" class="reply-icon" />
                                   <p class="font-weight-600">Edit</p>
                               </span>
-                              <span class="d-flex align-items-center cursor-pointer" onclick="onReplyClick(event)">
-                                  <img src="./assets/reply-icon.png" alt="" class="reply-icon" />
-                                  <p class="font-weight-600">Reply</p>
-                              </span>
+                              ${!offset && replyIcon()}
                           </span>
                       </div>
                       <div class="comment-text">${content}</div>
@@ -220,6 +350,22 @@ const comment = (author, date, content, likes, offset = 0) => {
               </div>
           </div>
     `;
+};
+
+const replyIcon = () => {
+  return `
+ <span
+   class="d-flex align-items-center cursor-pointer"
+   onclick="onReplyClick(event)"
+ >
+   <img
+     src="./assets/reply-icon.png"
+     alt=""
+     class="reply-icon"
+   />
+   <p class="font-weight-600">Reply</p>
+ </span>
+`;
 };
 
 const blogPosts = (
@@ -248,9 +394,14 @@ const blogPosts = (
                 ${content}
             </p>
             <div class="d-flex gap-2 flex-row flex-wrap">
-            ${tags.map(tag=>{
-              return `<h6 class="tag">#${tag}</h6>`
-            }).join("")}
+            ${
+              tags &&
+              tags
+                .map((tag) => {
+                  return `<h6 class="tag">#${tag}</h6>`;
+                })
+                .join("")
+            }
             </div>
              <div id="comment-section-${id}" class="comments-section row d-none">
                 ${commentsHTML}
